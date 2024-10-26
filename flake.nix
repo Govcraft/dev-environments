9fb725ef006c689bd86f98b76e1b58c33cedf9bd
Roland Rodriguez <roland@govcraft.ai>
2024-10-26 18:32:24 -0500

    fix:export flakes

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -17,5 +20,8 @@
       ];
 
       systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+      flakeModules.rustModule = ./modules/rust; # Add this line
+      exports.rustModule = ./modules/rust;      # And this line
     };
 }
