27e594c240a772844c5ddcdb5083329cec881194
Roland Rodriguez <roland@govcraft.ai>
2024-12-06 10:14:14 -0600

    feat: add golang env

--- modules/golang/default.nix
+++ modules/golang/default.nix
@@ -1,67 +1,85 @@
 {
-  pkgs,
   config,
+  flake-parts-lib,
   lib,
+  pkgs,
   ...
 }:
-
 let
-  inherit (config.go-dev)
-    enable
-    goVersion
-    withTools
-    extraPackages
+  inherit (flake-parts-lib)
+    mkPerSystemOption
+    ;
+  inherit (lib)
+    mkOption
+    types
     ;
 in
 {
-  options.go-dev = {
-    enable = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Enable/disable the Go development environment.";
-    };
+  options.perSystem = mkPerSystemOption (
+    {
+      config,
+      self',
+      inputs',
+      pkgs,
+      system,
+      ...
+    }:
+    let
+      mainSubmodule = types.submodule (
+        { config, ... }:
+        {
+          options = {
+            enable = lib.mkEnableOption "Golang development environment";
 
-    goVersion = lib.mkOption {
-      type = lib.types.enum [
-        "1.18"
-        "1.19"
-        "1.20"
-        "1.21"
-        "1.22"
-      ]; # List of supported versions
-      default = "1.23";
-      description = "Specify the Go version to use.";
-    };
+            goVersion = lib.mkOption {
+              type = lib.types.enum [
+                "1.18"
+                "1.19"
+                "1.20"
+                "1.21"
+                "1.22"
+                "1.23"
+              ];
+              default = "1.23";
+              description = "Golang toolchain version to use";
+            };
 
-    withTools = lib.mkOption {
-      type = lib.types.listOf lib.types.str;
-      default = [ ];
-      description = ''
-        List of Go tools to include. Use the official tool names (e.g., "golint", "gopls").
-      '';
-    };
+            withTools = lib.mkOption {
+              type = lib.types.listOf lib.types.str;
+              default = [ ];
+              description = "List of Golang tools to include (e.g., gopls, golint)";
+            };
 
-    extraPackages = lib.mkOption {
-      type = lib.types.listOf lib.types.package;
-      default = [ ];
-      description = "Additional packages to include in the Go environment.";
-    };
-  };
+            extraPackages = lib.mkOption {
+              type = lib.types.listOf lib.types.package;
+              default = [ ];
+              description = "Additional packages to include";
+            };
+          };
+        }
+      );
+    in
+    {
+      options.go-dev = lib.mkOption {
+        type = mainSubmodule;
+        description = lib.mdDoc ''
 
-  config = lib.mkIf enable {
-    env-packages =
-      with pkgs;
-      [
-        (pkgs.golang."${goVersion}")
-      ]
-      ++ lib.optionals (withTools != [ ]) (map (tool: pkgs.${tool}) withTools)
-      ++ extraPackages;
+                      Specification for the Golang development environment
+        '';
+        default = { };
+      };
 
-    env-hooks = [
-      ''
-        export GOPATH=${config.home.profileDirectory}/go
-        export PATH=$GOPATH/bin:$PATH
-      ''
-    ];
-  };
+      config = lib.mkIf config.go-dev.enable {
+        env-packages.go = [
+          (pkgs.golang."${config.go-dev.goVersion}")
+        ] ++ (map (tool: pkgs."${tool}") config.go-dev.withTools) ++ config.go-dev.extraPackages;
+
+        env-hooks.go = ''
+
+                      export GOPATH=$PWD/go
+                      export PATH=$GOPATH/bin:$PATH
+        '';
+      };
+    }
+  );
 }
