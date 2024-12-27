0524837819f63a4093b70b2ef4790ebd9f170134
Roland Rodriguez <roland@govcraft.ai>
2024-12-26 18:14:50 -0600

    feat: add typst

--- modules/typst/default.nix
+++ modules/typst/default.nix
@@ -1,60 +1,88 @@
-{ config, flake-parts-lib, lib, pkgs, ... }:
+{
+  config,
+  flake-parts-lib,
+  lib,
+  pkgs,
+  ...
+}:
 let
   inherit (flake-parts-lib)
-    mkPerSystemOption;
+    mkPerSystemOption
+    ;
   inherit (lib)
     mkOption
-    types;
+    types
+    ;
 in
 {
-  options.perSystem = mkPerSystemOption
-    ({ config, self', inputs', pkgs, system, ... }:
-      let
-        mainSubmodule = types.submodule ({ config, ... }: {
+  options.perSystem = mkPerSystemOption (
+    {
+      config,
+      self',
+      inputs',
+      pkgs,
+      system,
+      ...
+    }:
+    let
+      mainSubmodule = types.submodule (
+        { config, ... }:
+        {
           options = {
             enable = lib.mkEnableOption "Typst development environment";
-            
+
             withTools = lib.mkOption {
               type = lib.types.listOf lib.types.str;
-              default = [ "typst-fmt" "typst-lsp" ];
+              default = [
+                "typst-fmt"
+                "typst-lsp"
+              ];
               description = "List of Typst tools to include";
             };
-            
+
             extraPackages = lib.mkOption {
               type = lib.types.listOf lib.types.package;
               default = [ ];
               description = "Additional packages to include";
             };
-            
+
             ide = {
               type = lib.mkOption {
-                type = lib.types.enum [ "vscode" "none" ];
+                type = lib.types.enum [
+                  "vscode"
+                  "none"
+                ];
                 default = "none";
                 description = "IDE preference for Typst development";
               };
             };
           };
-        });
-      in
-      {
-        options.typst-dev = lib.mkOption {
-          type = mainSubmodule;
-          description = lib.mdDoc ''
-            Specification for the Typst development environment
-          '';
-          default = { };
-        };
-        config = lib.mkIf config.typst-dev.enable {
-          env-packages.typst = [
+        }
+      );
+    in
+    {
+      options.typst-dev = lib.mkOption {
+        type = mainSubmodule;
+        description = lib.mdDoc ''
+          Specification for the Typst development environment
+        '';
+        default = { };
+      };
+      config = lib.mkIf config.typst-dev.enable {
+        env-packages.typst =
+          [
             pkgs.typst
-          ] ++ lib.optionals (config.typst-dev.ide.type == "vscode") [
+          ]
+          ++ lib.optionals (config.typst-dev.ide.type == "vscode") [
             pkgs.typst-lsp
-          ] ++ (map (tool: pkgs.${tool}) config.typst-dev.withTools) 
-            ++ config.typst-dev.extraPackages;
-          
-          env-hooks.typst = ''
-            echo "Typst development environment activated"
-          '';
-        };
-      });
+          ]
+          ++ (map (tool: pkgs.${tool}) config.typst-dev.withTools)
+          ++ config.typst-dev.extraPackages;
+
+        env-hooks.typst = ''
+          echo "Typst development environment activated"
+        '';
+      };
+    }
+  );
 }
